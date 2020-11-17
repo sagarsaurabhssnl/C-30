31c52d9f75bad5f81b52165b631d3bc9500a12a5
sagarsaurabhssnl <56288082+sagarsaurabhssnl@users.noreply.github.com>
2020-11-17 14:41:12 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,26 @@
+class Bird {
+  constructor(x, y) {
+    var options = {
+        'restitution':0.8,
+        'friction':1.0,
+        'density':3.0
+    }
+    this.body = Bodies.rectangle(x, y, 50, 50, options);
+    this.width = 50;
+    this.height = 50;
+    World.add(world, this.body);
+    this.image = loadImage("sprites/bird.png");
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+    var angle = this.body.angle;
+    push();
+    translate(this.body.position.x, this.body.position.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image, 0, 0, this.width, this.height);
+    pop();
+  }
+}
